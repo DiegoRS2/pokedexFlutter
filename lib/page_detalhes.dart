6dbedf223e916b99a378010eb2ba3b5b5646f00a
DiegoRS2 <72531642+DiegoRS2@users.noreply.github.com>
2023-01-05 22:34:09 -0300

    Update page_detalhes.dart

--- lib/page_detalhes.dart
+++ lib/page_detalhes.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'components/descricao_pokemon.dart';
+
 class PageDetalhes extends StatefulWidget {
   final pokemonSelecionado;
   Color cor;
@@ -32,13 +34,31 @@ class _PageDetalhesState extends State<PageDetalhes> {
           Align(
             alignment: Alignment.topLeft,
             child: Padding(
-              padding: const EdgeInsets.only(top: 8, left: 8),
-              child: Text(
-                widget.pokemonSelecionado['name'],
-                style: const TextStyle(
-                    fontSize: 30,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold),
+              padding: const EdgeInsets.only(top: 8, left: 8, bottom: 8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    widget.pokemonSelecionado['name'],
+                    style: const TextStyle(
+                        fontSize: 30,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Container(
+                    decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        color: Colors.black26),
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                          left: 8, right: 8, top: 4, bottom: 4),
+                      child: Text(
+                        widget.pokemonSelecionado['type'].join(', '),
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  )
+                ],
               ),
             ),
           ),
@@ -46,16 +66,42 @@ class _PageDetalhesState extends State<PageDetalhes> {
             alignment: Alignment.bottomCenter,
             child: Container(
               width: width,
-              height: height * 0.6,
-              decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(30),
-                      topRight: Radius.circular(30)),
-                  color: Colors.white),
+              height: height * 0.8,
+              child: DefaultTabController(
+                length: 3, // número de tabs
+                child: Scaffold(
+                  appBar: AppBar(
+                    automaticallyImplyLeading: false,
+                    backgroundColor: widget.cor,
+                    bottom: const TabBar(
+                      tabs: [
+                        Tab(text: 'Sobre'),
+                        Tab(text: 'estatísticas'),
+                        Tab(text: 'Evoluções')
+                      ],
+                    ),
+                  ),
+                  body: TabBarView(
+                    children: [
+                      // conteúdo da primeira tab
+                      Center(child: DescricaoPokemon(
+                      nome: widget.pokemonSelecionado['name'],
+                      altura: widget.pokemonSelecionado['height'].toString(),
+                      chance: widget.pokemonSelecionado['spawn_chance'].toString(),
+                      fraqueza: widget.pokemonSelecionado['weaknesses'].join(', '),
+                      peso: widget.pokemonSelecionado['weight'].toString(),
+                      tempo: widget.pokemonSelecionado['spawn_time'].toString(),)),
+                      // conteúdo da segunda tab
+                      Center(child: Text('Conteúdo da Tab 2')),
+                      Center(child: Text('teste'),)
+                    ],
+                  ),
+                ),
+              ),
             ),
           ),
           Align(
-            alignment: Alignment.bottomRight,
+            alignment: Alignment.topRight,
             child: Image.network(widget.pokemonSelecionado['img'].toString()),
           ),
         ],
