b399938e85b882b3fc3c44a1f89d2f116d3d0066
DiegoRS2 <72531642+DiegoRS2@users.noreply.github.com>
2023-01-11 21:18:22 -0300

    função de pesquisa

--- lib/pagina_principal.dart
+++ lib/pagina_principal.dart
@@ -8,6 +8,7 @@ import 'components/card_component.dart';
 class PaginaPrincipal extends StatefulWidget {
   final jsonResponse = '';
   late List pokeApi;
+  late List pokeApiBackup = [];
   String type = '';
   PaginaPrincipal({super.key, required this.pokeApi});
 
@@ -29,92 +30,92 @@ class _PaginaPrincipalState extends State<PaginaPrincipal> {
         backgroundColor: Colors.transparent,
         elevation: 0,
         centerTitle: true,
-        title: const Text(
-          'Pokedex',
-          style: TextStyle(
-              color: Colors.black,
-              decoration: TextDecoration.none,
-              fontSize: 30),
-        ),
+        title: const Text('Pokedex',
+            style: TextStyle(
+              color: Colors.deepOrange,
+              fontWeight: FontWeight.w600,
+              fontSize: 30,
+              fontFamily: 'Pokemon',
+            )),
       ),
       body: Padding(
         padding: const EdgeInsets.all(10),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Expanded(
-              child: ListView.builder(
-                itemCount: widget.pokeApi.length,
-                itemBuilder: (BuildContext context, int index) {
-                  widget.type = widget.pokeApi[index]['type'][0];
-                  return InkWell(
-                    onTap: () {
-                      widget.type = widget.pokeApi[index]['type'][0];
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => PageDetalhes(pokemonSelecionado: widget.pokeApi[index], cor: selecionaCor()),
-                      ),);
-                    },
-                    child: CardComponent(
-                        nome: widget.pokeApi[index]['name'].toString(),
-                        id: widget.pokeApi[index]['id'].toString(),
-                        img: widget.pokeApi[index]['img'].toString(),
-                        cor: selecionaCor()),
-                  );
-                },
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    child: TextField(
+                      onChanged: (value) {
+                        searchPokemon(value);
+                      },
+                      decoration: const InputDecoration(
+                          labelText: "Search",
+                          hintText: "Search",
+                          prefixIcon: Icon(Icons.search),
+                          border: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(25.0)))),
+                    ),
+                  ),
+                  const SizedBox( height: 20,),
+                  Expanded(
+                    child: widget.pokeApi.isEmpty
+                        ? Center(
+                            child: Text(
+                              "Pokemon not found!",
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.deepOrange[900],
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          )
+                        : ListView.builder(
+                            itemCount: widget.pokeApi.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              widget.type = widget.pokeApi[index]['type'][0];
+                              return InkWell(
+                                onTap: () {
+                                  widget.type =
+                                      widget.pokeApi[index]['type'][0];
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => PageDetalhes(
+                                          pokemonSelecionado:
+                                              widget.pokeApi[index],
+                                          cor: selecionaCor()),
+                                    ),
+                                  );
+                                },
+                                child: CardComponent(
+                                    nome: widget.pokeApi[index]['name']
+                                        .toString(),
+                                    id: widget.pokeApi[index]['id'].toString(),
+                                    img:
+                                        widget.pokeApi[index]['img'].toString(),
+                                    cor: selecionaCor()),
+                              );
+                            },
+                          ),
+                  )
+                ],
               ),
-            )
-          ],
-        ),
       ),
     );
   }
 
-  Color selecionaCor(){
-    return widget.type == 'Grass'
-    ? const Color.fromARGB(255, 127, 247, 131)
-    : widget.type == "Fire"
-    ? const Color.fromARGB(255, 241, 97, 86)
-    : widget.type == "Water"
-    ? const Color.fromARGB(255, 108, 187, 252)
-    : widget.type == "Electric"
-    ? const Color.fromARGB(
-    255, 241, 227, 99)
-    : widget.type == "Rock"
-    ? const Color.fromARGB(
-    255, 235, 232, 232)
-    : widget.type == "Ground"
-    ? const Color.fromARGB(
-    255, 143, 108, 95)
-    : widget.type == "Physic"
-    ? const Color.fromARGB(
-    255, 90, 105, 189)
-    : widget.type == "Fighting"
-    ? const Color.fromARGB(
-    255, 248, 175, 66)
-    : widget.type == "Bug"
-    ? Colors.lightGreen
-    : widget.type == "Ghost"
-    ? const Color
-    .fromARGB(
-    255,
-    121,
-    85,
-    184)
-    : widget.type ==
-    "Normal"
-    ? const Color
-    .fromARGB(
-    66,
-    82,
-    81,
-    81)
-    : const Color
-    .fromARGB(
-    255,
-    214,
-    75,
-    121);
+  void searchPokemon(String search) {
+    setState(() {
+      if (search.isEmpty) {
+        widget.pokeApi = widget.pokeApiBackup;
+      } else {
+        widget.pokeApi = widget.pokeApiBackup
+            .where((pokemon) =>
+                pokemon['name'].toLowerCase().contains(search.toLowerCase()))
+            .toList();
+      }
+    });
   }
 
   void fetchPOkemon() async {
@@ -122,8 +123,38 @@ class _PaginaPrincipalState extends State<PaginaPrincipal> {
         'https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json');
     await http.get(uri).then((value) {
       var jsonResponse = jsonDecode(value.body);
-      widget.pokeApi = jsonResponse['pokemon'];
+      widget.pokeApiBackup = jsonResponse['pokemon'];
+      widget.pokeApi = widget.pokeApiBackup;
       setState(() {});
     });
+  }
+
+  Color selecionaCor() {
+    return widget.type == 'Grass'
+        ? const Color.fromARGB(255, 127, 247, 131)
+        : widget.type == "Fire"
+            ? const Color.fromARGB(255, 241, 97, 86)
+            : widget.type == "Water"
+                ? const Color.fromARGB(255, 108, 187, 252)
+                : widget.type == "Electric"
+                    ? const Color.fromARGB(255, 241, 227, 99)
+                    : widget.type == "Rock"
+                        ? const Color.fromARGB(255, 235, 232, 232)
+                        : widget.type == "Ground"
+                            ? const Color.fromARGB(255, 143, 108, 95)
+                            : widget.type == "Physic"
+                                ? const Color.fromARGB(255, 90, 105, 189)
+                                : widget.type == "Fighting"
+                                    ? const Color.fromARGB(255, 248, 175, 66)
+                                    : widget.type == "Bug"
+                                        ? Colors.lightGreen
+                                        : widget.type == "Ghost"
+                                            ? const Color.fromARGB(
+                                                255, 121, 85, 184)
+                                            : widget.type == "Normal"
+                                                ? const Color.fromARGB(
+                                                    66, 82, 81, 81)
+                                                : const Color.fromARGB(
+                                                    255, 214, 75, 121);
   }
 }
