fe60022ec8f6a8c591ffae949e0c5dd9e21b6eee
DiegoRS2 <72531642+DiegoRS2@users.noreply.github.com>
2023-01-13 00:20:15 -0300

    Update page_detalhes.dart

--- lib/page_detalhes.dart
+++ lib/page_detalhes.dart
@@ -1,9 +1,12 @@
+import 'dart:convert';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
-
+import 'package:http/http.dart' as http;
 import 'components/descricao_pokemon.dart';
 
 class PageDetalhes extends StatefulWidget {
   final pokemonSelecionado;
+  List<Map<String, dynamic>> _pokemonStats = [];
   Color cor;
   PageDetalhes(
       {super.key, required this.pokemonSelecionado, required this.cor});
@@ -13,6 +16,12 @@ class PageDetalhes extends StatefulWidget {
 }
 
 class _PageDetalhesState extends State<PageDetalhes> {
+  @override
+  void initState() {
+    super.initState();
+    fetchPokemonStats(widget.pokemonSelecionado['id'].toString());
+  }
+
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
@@ -45,7 +54,9 @@ class _PageDetalhesState extends State<PageDetalhes> {
                         color: Colors.white,
                         fontWeight: FontWeight.bold),
                   ),
-                  const SizedBox(height: 10,),
+                  const SizedBox(
+                    height: 10,
+                  ),
                   Container(
                     decoration: const BoxDecoration(
                         borderRadius: BorderRadius.all(Radius.circular(10)),
@@ -69,7 +80,7 @@ class _PageDetalhesState extends State<PageDetalhes> {
               width: width,
               height: height * 0.8,
               child: DefaultTabController(
-                length: 3, // número de tabs
+                length: 2, // número de tabs
                 child: Scaffold(
                   appBar: AppBar(
                     automaticallyImplyLeading: false,
@@ -78,25 +89,57 @@ class _PageDetalhesState extends State<PageDetalhes> {
                       tabs: [
                         Tab(text: 'Sobre'),
                         Tab(text: 'estatísticas'),
-                        Tab(text: 'Evoluções')
                       ],
                     ),
                   ),
                   body: TabBarView(
                     children: [
                       // conteúdo da primeira tab
-                      Center(child: DescricaoPokemon(
-                      nome: widget.pokemonSelecionado['name'],
-                      altura: widget.pokemonSelecionado['height'].toString(),
-                      chance: widget.pokemonSelecionado['spawn_chance'].toString(),
-                      fraqueza: widget.pokemonSelecionado['weaknesses'].join(', '),
-                      peso: widget.pokemonSelecionado['weight'].toString(),
-                      tempo: widget.pokemonSelecionado['spawn_time'].toString(),
-                      next_evolution: widget.pokemonSelecionado['next_evolution'],
+                      Center(
+                          child: DescricaoPokemon(
+                        nome: widget.pokemonSelecionado['name'],
+                        altura: widget.pokemonSelecionado['height'].toString(),
+                        chance: widget.pokemonSelecionado['spawn_chance']
+                            .toString(),
+                        fraqueza:
+                            widget.pokemonSelecionado['weaknesses'].join(', '),
+                        peso: widget.pokemonSelecionado['weight'].toString(),
+                        tempo:
+                            widget.pokemonSelecionado['spawn_time'].toString(),
+                        next_evolution:
+                            widget.pokemonSelecionado['next_evolution'],
                       )),
                       // conteúdo da segunda tab
-                      Center(child: Text('Conteúdo da Tab 2')),
-                      Center(child: Text('teste'),)
+                      Center(
+                          child: ListView.builder(
+                        itemCount: widget._pokemonStats.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Padding(
+                              padding: const EdgeInsets.all(12),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    '${widget._pokemonStats[index]['stat']['name']} - ${widget._pokemonStats[index]['base_stat'].toString()}',
+                                    style: const TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  SizedBox(
+                                    width: 200,
+                                    child: LinearProgressIndicator(
+                                      minHeight: 10,
+                                      value: widget._pokemonStats[index]
+                                              ['base_stat'] /
+                                          100,
+                                    ),
+                                  ),
+                                ],
+                              ));
+                        },
+                      )),
                     ],
                   ),
                 ),
@@ -110,5 +153,18 @@ class _PageDetalhesState extends State<PageDetalhes> {
         ],
       ),
     );
+  }
+
+  void fetchPokemonStats(String pokeId) async {
+    final response =
+        await http.get(Uri.parse('https://pokeapi.co/api/v2/pokemon/$pokeId'));
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      widget._pokemonStats = List<Map<String, dynamic>>.from(data['stats']);
+      setState(() {});
+    } else {
+      throw Exception('Failed to load pokemon stats');
+    }
   }
 }
